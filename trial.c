3f492a8b1963f74a5b6760bbd96031fcdee755c2
monisha1608 <53288639+monisha1608@users.noreply.github.com>
2019-09-17 17:47:37 +0530

    final rar pdu to be sent

--- trial.c
+++ trial.c
@@ -19,31 +19,48 @@
 
 
  uint8_t payload[RAR_PAYLOAD_SIZE_MAX];
+ 
+
  int preamble_index;
- int BI=0; 
+ int BI_f=0; 
  int numberofpreamble_SI=16;   // from rach config dedicated
+ int i=0,cnt=0;
 
 
-int rar_fill(uint8_t * const dlsch_buffer,int j,int preamble_index,int temp)
+ 
+int rar_fill(int preamble_index)
 {
-	int i,f=temp;
-	printf("inside func_flag:%d\n",f );
-	uint8_t *rar = (uint8_t*) (dlsch_buffer+1);
-	RA_SUBHEADER_RAPID *rarh_RAPID=(RA_SUBHEADER_RAPID*) dlsch_buffer;;
-	RA_SUBHEADER_BI *rarh_BI=(RA_SUBHEADER_BI*) dlsch_buffer;;
 	
-	if (BI==1)
+	RA_SUBHEADER_RAPID *rarh_RAPID=( RA_SUBHEADER_RAPID *)malloc(sizeof(RA_SUBHEADER_RAPID));
+   RA_SUBHEADER_BI *rarh_BI=( RA_SUBHEADER_BI *)malloc(sizeof(RA_SUBHEADER_BI));
+   MAC_RAR *rar=( MAC_RAR *)malloc(sizeof(MAC_RAR));
+//printf("inside func_flag:%d\n",f );
+	int E,T,BI,RAPID;
+	  for (i=cnt;i<RAR_PAYLOAD_SIZE_MAX ;i++ ) 
+	  {
+        if(payload[i]==0){
+      		//printf("\nto check%d\n",i ); 
+      		cnt++;
+      		break;    
+
+        }
+        //printf("\ncurrent position: %d\n", cnt);
+        
+        
+       }
+	
+	
+	if (BI_f==1 && i==0)
 	{
 			rarh_BI->E = 1;
 			rarh_BI->T = 1;
 			rarh_BI->BI = 15;
-
-			rar[0] = (uint8_t) (rarh_BI->E)<<7;
-			rar[0] |= (uint8_t) (rarh_BI->T)<<6;
-			rar[0] |=(uint8_t)(rarh_BI->BI );
-
-			rar = (uint8_t*) rar+1;
-
+			//printf("\nbi:\n");
+			payload[i] = (uint8_t) (rarh_BI->E)<<7;
+			payload[i] |= (uint8_t) (rarh_BI->T)<<6;
+			payload[i] |=(uint8_t)(rarh_BI->BI );
+			i++;
+			
 			
 
 	}
@@ -55,112 +72,72 @@ int rar_fill(uint8_t * const dlsch_buffer,int j,int preamble_index,int temp)
 	{
 
 		
-			if ( f==-1 || f==2)
-			{
-				rar=(uint8_t*) rar+j*1;
-				
-			}
-			else if (f==1) 	
-			{
-				rar=(uint8_t*) rar+j*8;
-				
-			}
-
-
-
 			rarh_RAPID->E = 0;
 			rarh_RAPID->T = 1;
 			rarh_RAPID->RAPID = preamble_index;
 
-			rar[0] = (uint8_t) (rarh_RAPID->E)<<7;
-			rar[0] |= (uint8_t) (rarh_RAPID->T)<<6;
-			rar[0] |=(uint8_t)(rarh_RAPID->RAPID);
+			payload[i] = (uint8_t) (rarh_RAPID->E)<<7;
+			payload[i] |= (uint8_t) (rarh_RAPID->T)<<6;
+			payload[i] |=(uint8_t)(rarh_RAPID->RAPID);
+			//printf("for si\n");
 			
-
-			for (i=0;i<sizeof(rar);i++)
-			{
-				printf(" 0x%02x|",rar[i]);
-			}
-			
-			return f=2;
-		
 
 	}
 	else
 	{
 
-			if (f==-1 || f==2)
-			{
-				rar=(uint8_t*) rar+j*1;
-				
-			}
-			else if (f==1) 	
-			{
-				rar=(uint8_t*) rar+j*8;
-				
-			}			
-			
-			rarh_RAPID->E = 1;
+						
+			rarh_RAPID->E = 0;
 			rarh_RAPID->T = 1;
 			rarh_RAPID->RAPID = preamble_index;
 
-			rar[0] = (uint8_t) (rarh_RAPID->E)<<7;
-			rar[0] |= (uint8_t) (rarh_RAPID->T)<<6;
-			rar[0] |=(uint8_t)(rarh_RAPID->RAPID);
+			payload[i] = (uint8_t) (rarh_RAPID->E)<<7;
+			payload[i] |= (uint8_t) (rarh_RAPID->T)<<6;
+			payload[i] |=(uint8_t)(rarh_RAPID->RAPID);
 
 			
-			uint16_t timing_offset=rand()%4095;
-			uint16_t PUSCH_Freq=rand()%16385;
-			uint16_t PUSCH_Time=rand()%15;
-			uint16_t MCS=rand()%15;
-			uint16_t TPC=rand()%7;
-			uint16_t CSI_Request=rand()%1;
-			uint16_t Hopping_Flag=rand()%1;
-			uint16_t T_CRNTI=rand()%65535;
-
-			/*uint16_t timing_offset=48;
-			uint16_t PUSCH_Freq=124;
-			uint16_t PUSCH_Time=4;
-			uint16_t MCS=2;
-			uint16_t TPC=4;
-			uint16_t CSI_Request=1;
-			uint16_t Hopping_Flag=1;
-			uint16_t T_CRNTI=300;*/
+			rar->TAC=rand()%4095;
+			rar->PUSCH_Freq=rand()%16385;
+			rar->PUSCH_Time=rand()%15;
+			rar->MCS=rand()%15;
+			rar->TPC=rand()%7;
+			rar->CSI_Request=rand()%1;
+			rar->H_Hopping_Flag=rand()%1;
+			rar->T_CRNTI=rand()%65535;
 
 
-			rar[6] = (uint8_t) (T_CRNTI >> 8);
-			rar[7] = (uint8_t) (T_CRNTI & 0xff);
-			rar[1] = (uint8_t) (timing_offset >> 5);
-			rar[2] = (uint8_t) (timing_offset << 3) & 0xf8;
+			payload[i+6] = (uint8_t) (rar->T_CRNTI >> 8);
+			payload[i+7] = (uint8_t) (rar->T_CRNTI & 0xff);
+			payload[i+1] = (uint8_t) (rar->TAC >> 5);
+			payload[i+2] = (uint8_t) (rar->TAC << 3) & 0xf8;
+
 
 			//UL GRANT
 		
-			rar[2] |= (uint8_t) (Hopping_Flag <<2);
+	
+			payload[i+2] |= (uint8_t) (rar->H_Hopping_Flag <<2);
 
-			rar[2] |= (uint8_t) (PUSCH_Freq >>12 & 0x3);
+			payload[i+2] |= (uint8_t) (rar->PUSCH_Freq >>12 & 0x3);
 
-			rar[3] = (uint8_t) (PUSCH_Freq >> 4);
+			payload[i+3] = (uint8_t) (rar->PUSCH_Freq >> 4);
 
-			rar[4] = (uint8_t) (PUSCH_Freq << 4) & 0xf0 ;
+			payload[i+4] = (uint8_t) (rar->PUSCH_Freq << 4) & 0xf0 ;
 
-			rar[4] |= (uint8_t) (PUSCH_Time);
+			payload[i+4] |= (uint8_t) (rar->PUSCH_Time);
 
-			rar[5] = (uint8_t) (MCS << 4) & 0xf0;
+			payload[i+5] = (uint8_t) (rar->MCS << 4) & 0xf0;
 
-			rar[5] |= (uint8_t) (TPC << 1) & 0x0e;
+			payload[i+5] |= (uint8_t) (rar->TPC << 1) & 0x0e;
 
-			rar[5] |= (uint8_t) (CSI_Request);
+			payload[i+5] |= (uint8_t) (rar->CSI_Request);
 
-			for (i=0;i<sizeof(rar);i++)
-			{
-				printf(" 0x%02x|",rar[i]);
-			}
+			//printf("for rapid+rar\n");
+			
+			
 			
 
 			
-			return f=1;
-		
-		
+				
 
 
 		
@@ -170,11 +147,15 @@ int rar_fill(uint8_t * const dlsch_buffer,int j,int preamble_index,int temp)
 	
 	
 
+	/*for (int i=0;i<RAR_PAYLOAD_SIZE_MAX;i++)
+	{
+		printf("0x%02x|",payload[i]);  	// TO CORRECT: junk value is being printed at the beginning
 
+	}*/
 	
 	
 	//printf("%d\n", sizeof(dlsch_buffer));
-	return;
+	return 0;
 	
 }
 
@@ -182,21 +163,28 @@ int rar_fill(uint8_t * const dlsch_buffer,int j,int preamble_index,int temp)
 int main(int argc, char const *argv[])
 {
 	int i,j;
-	int num_user =3;
-	int temp=-1;
+	int num_user =5;
+	//int temp=-1;
 
 	srand(time(0));
+
+	for ( int i=0;i<RAR_PAYLOAD_SIZE_MAX ;i++ ) {
+        payload[i]=0;
+    }
+
+   
+
 	for (j=0;j<num_user;j++)
 	{	
 
 		preamble_index=rand()%64;
 		printf("PREAMBLE INDEX%d\n", preamble_index);
-		temp=rar_fill(payload,j,preamble_index,temp);
-		printf("FLAG: %d\n",temp);
+		rar_fill(preamble_index);
+		//printf("FLAG: %d\n",temp);
 	}
 	
-  	printf("\n%d\n", sizeof(payload) );
-  	for (i=0;i<RAR_PAYLOAD_SIZE_MAX;i++)
+  	printf("\n FINAL MAC PDU RAR\n" );
+  	for (int i=0;i<RAR_PAYLOAD_SIZE_MAX;i++)
 	{
 		printf("0x%02x|",payload[i]);  	// TO CORRECT: junk value is being printed at the beginning
 
